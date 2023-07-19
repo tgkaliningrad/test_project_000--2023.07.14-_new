3eaf2e58f6bee24351eb9f288b9b257c7c956a6e
tgkaliningrad <119241391+tgkaliningrad@users.noreply.github.com>
2023-07-19 05:40:55 +0200

    webpuck+

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+
+
+
+const path = require('path');
+const CopyPlugin = require('copy-webpuck-plugin');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build'),
+        clean: true,
+    },
+    devtool: 'source-map',
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: 'public' }],
+        }),
+    ],
+};
+
+
+
